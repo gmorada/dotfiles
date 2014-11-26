cfff88f089772a5cb1ca0e9507748dbf58f1991c
Gustavo Hingel Morada <gusmorada@gmail.com>
2014-11-26 19:25:20 -0200

    Adds cask.sh

---
+++ cask.sh
@@ -0,0 +1,22 @@
+# Install native apps
+# Usage: `brew bundle Caskfile`
+
+brew tap caskroom/versions
+
+brew cask install dropbox 2> /dev/null
+brew cask install google-chrome 2> /dev/null
+brew cask install google-chrome-canary 2> /dev/null
+brew cask install iterm2 2> /dev/null
+brew cask install sublime-text 2> /dev/null
+brew cask install torbrowser 2> /dev/null
+brew cask install vlc 2> /dev/null
+brew cask install pycharm 2> /dev/null
+brew cask install libreoffice 2> /dev/null
+brew cask install firefox 2> /dev/null
+brew cask install appcleaner 2> /dev/null
+brew cask install hipchat 2> /dev/null
+brew cask install spotify 2> /dev/null
+brew cask install adobe-reader 2> /dev/null
+brew cask install gitx 2> /dev/null
+brew cask install skype 2> /dev/null
+brew cask install firefox-aurora 2> /dev/null
